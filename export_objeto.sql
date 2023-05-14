ef259aedb872c1d0d943e12bee89fe76294370cf
“John <johndoe@example.com>
2023-05-14 14:37:50 +0200

    Update export_objeto.sql

--- export_objeto.sql
+++ export_objeto.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: mydb
+-- Host: localhost    Database: proyecto
 -- ------------------------------------------------------
 -- Server version	10.4.28-MariaDB
 
