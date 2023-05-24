4f39fc604fb85945965ad698523fd08736b8bf98
“John <johndoe@example.com>
2023-05-24 21:02:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ use \model\CajasM;
 use \model\Utils;
 
 
-require_once("/model/UsuarioM.php");
-require_once("/model/Utils.php");
-require_once("/model/CajasM.php");
+require_once("model/UsuarioM.php");
+require_once("model/Utils.php");
+require_once("model/CajasM.php");
 
 
 // En otros controladores
