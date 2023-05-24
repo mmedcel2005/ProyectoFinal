c3eb078c5add655de0ace476a7d4df914690c8ff
“John <johndoe@example.com>
2023-05-24 21:00:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,27 @@ namespace controller;
 
 
 use \model\UsuarioM;
+use \model\CajasM;
 use \model\Utils;
+
+
+require_once("../model/UsuarioM.php");
+require_once("../model/Utils.php");
+require_once("../model/CajasM.php");
 
 
 // En otros controladores
 session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     // El usuario ha iniciado sesión, permitir acceso a la página
+
+    
+    $gestorCaj = new CajasM();
+    
+    $datosCajas= $gestorCaj->obtenerCajas($conexPDO);
+
+
+
     include("views/inicioV.php");
 } else {
     // El usuario no ha iniciado sesión, redirigir a la página de inicio de sesión
