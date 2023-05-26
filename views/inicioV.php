219e40db9886860b57ce26fe0be286dca2e656b3
“John <johndoe@example.com>
2023-05-26 17:47:14 +0200

    Update inicioV.php

--- views/inicioV.php
+++ views/inicioV.php
@@ -91,14 +91,25 @@
             <div class="card mb-4 bg-custom-sec">
               <div class="card-body text-center bg-custom-sec ">
                 <div class="d-flex justify-content-between ">
-                <form action="../index.php" method="post">
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1">POPULAR</button>
-                  
+                  </form>
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1" id="categoria" value="T">TECNOLOGÍA</button>
+                  </form>
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1" id="categoria" value="R">ROPA</button>
+                  </form>
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1" id="categoria" value="F">FIGURAS</button>
+                  </form>
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1" id="categoria" value="A">ACCESORIOS</button>
+                  </form>
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1" id="categoria" value="N">NUEVO</button>
+                  </form>
+                  <form action="../index.php" method="post">
                   <button class="btn bg-custom flex-fill mx-2 pt-2 pb-2 text-muted rounded-1" id="categoria" value="O">OFERTAS</button>
                   </form>
                 </div>
