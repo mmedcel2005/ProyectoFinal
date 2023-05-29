d59d0d82e959780e20731d71eb8c2f91b687a08a
“John <johndoe@example.com>
2023-05-29 18:57:08 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -1,201 +1,95 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Ruleta con Bootstrap</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
-  <link rel="stylesheet" href="styles.css">
-
-  <style>
-    .container {
-  margin-top: 50px;
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+    <style> .container {
+    margin-top: 50px;
+    text-align: center;
 }
 
-h1 {
-  text-align: center;
+#carouselExampleControls {
+    width: 400px;
+    margin: 0 auto;
 }
 
-.card {
-  width: 300px;
-  margin: 0 auto;
-}
-
-#spinBtn {
-  display: block;
-  margin: 20px auto;
-  text-align: center;
-}
-
-.modal {
-  display: none;
-  text-align: center;
-}
-
-.modal-content {
-  margin-top: 20px;
+#spinButton {
+    margin-top: 20px;
 }
 
 #winnerText {
-  font-size: 24px;
-  font-weight: bold;
-}
-
-  </style>
+    font-weight: bold;
+    font-size: 18px;
+    text-align: center;
+}</style>
+    
 </head>
 <body>
-<!-- Código HTML -->
-<div class="container">
-  <h1>Ruleta con bootstrap</h1>
-  <p>Pulsa el botón para girar la ruleta y ver el ítem ganador.</p>
-  <button id="spin-button" class="btn btn-primary">Girar</button>
-  <div id="roulette" class="carousel fade mt-4" data-bs-ride="carousel" data-bs-interval="false">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <div class="card bg-primary text-white">
-          <div class="card-body">
-            <h5 class="card-title">Ítem 1</h5>
-            <p class="card-text">Este es el ítem 1.</p>
-          </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Ruleta con Estilo de Carousel</h1>
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="item1.jpg" class="d-block w-100" alt="Item 1">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="item2.jpg" class="d-block w-100" alt="Item 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="item3.jpg" class="d-block w-100" alt="Item 3">
+                        </div>
+                        <!-- Agrega más elementos de carousel según sea necesario -->
+                    </div>
+                </div>
+                <button id="spinButton" class="btn btn-primary">Girar Ruleta</button>
+            </div>
         </div>
-      </div>
-      <div class="carousel-item">
-        <div class="card bg-secondary text-white">
-          <div class="card-body">
-            <h5 class="card-title">Ítem 2</h5>
-            <p class="card-text">Este es el ítem 2.</p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="card bg-success text-white">
-          <div class="card-body">
-            <h5 class="card-title">Ítem 3</h5>
-            <p class="card-text">Este es el ítem 3.</p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="card bg-danger text-white">
-          <div class="card-body">
-            <h5 class="card-title">Ítem 4</h5>
-            <p class="card-text">Este es el ítem 4.</p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="card bg-warning text-white">
-          <div class="card-body">
-            <h5 class="card-title">Ítem 5</h5>
-            <p class="card-text">Este es el ítem 5.</p>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 
-  <!-- Modal -->
-  <div id="winner-modal" class="modal fade" tabindex="-1" aria-labelledby="winner-modal-label" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 id="winner-modal-label" class="modal-title">¡Felicidades!</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div id="winnerModal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">¡Felicidades!</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p id="winnerText"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
         </div>
-        <div id="winner-modal-body" class="modal-body">
-          <!-- Aquí se mostrará el ítem ganador -->
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        </div>
-      </div>
     </div>
-  </div>
 
-</div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
+    <script src="script.js">$(document).ready(function() {
+    // Inicializar el carousel
+    $('#carouselExampleControls').carousel({
+        interval: false
+    });
 
-<!-- Código CSS -->
-<style>
-  /* Estilos para el carousel */
-  #roulette {
-    width: 300px;
-    height: 300px;
-    margin: 0 auto;
-    border-radius: 50%;
-    overflow: hidden;
-    transform: rotate(45deg) scale(0.8);
-  }
+    // Obtener la cantidad de elementos en el carousel
+    var carouselItemsCount = $('#carouselExampleControls .carousel-item').length;
 
-  .carousel-item {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    // Girar la ruleta cuando se hace clic en el botón
+    $('#spinButton').click(function() {
+        var winnerIndex = Math.floor(Math.random() * carouselItemsCount); // Generar un índice aleatorio para seleccionar el ganador
+        $('#carouselExampleControls').carousel('pause'); // Pausar el carousel
+        $('#carouselExampleControls').carousel(winnerIndex); // Mover el carousel al índice del ganador
 
-  .card {
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    transform: rotate(-45deg);
-  }
-
-</style>
-
-<!-- Código JavaScript -->
-<script>
-  // Variables globales
-  var roulette = $("#roulette"); // El elemento carousel
-  var spinButton = $("#spin-button"); // El botón para girar la ruleta
-  var winnerModal = $("#winner-modal"); // El modal para mostrar el ítem ganador
-  var winnerModalBody = $("#winner-modal-body"); // El cuerpo del modal
-  var items = roulette.find(".carousel-item"); // Los ítems del carousel
-  var numItems = items.length; // El número de ítems
-  var spinning = false; // Indica si la ruleta está girando o no
-  var spinInterval; // El intervalo para girar la ruleta
-  var spinDuration = 5000; // La duración del giro en milisegundos
-  var spinSpeed = 50; // La velocidad del giro en milisegundos
-
-  // Función para girar la ruleta
-  function spinRoulette() {
-    if (!spinning) { // Si la ruleta no está girando, empezar a girarla
-      spinning = true; // Cambiar el estado a girando
-      spinButton.prop("disabled", true); // Deshabilitar el botón para evitar múltiples clicks
-      spinInterval = setInterval(function() { // Crear un intervalo para cambiar el ítem activo cada cierto tiempo
-        var activeItem = roulette.find(".carousel-item.active"); // Encontrar el ítem activo actual
-        var nextItem = activeItem.next(); // Encontrar el siguiente ítem
-        if (nextItem.length == 0) { // Si no hay más ítems, volver al primero
-          nextItem = items.first();
-        }
-        activeItem.removeClass("active"); // Quitar la clase activa al ítem actual
-        nextItem.addClass("active"); // Añadir la clase activa al siguiente ítem
-      }, spinSpeed); // Ejecutar el intervalo cada cierto tiempo según la velocidad del giro
-      setTimeout(function() { // Crear un temporizador para detener el giro después de cierta duración
-        clearInterval(spinInterval); // Limpiar el intervalo
-        spinning = false; // Cambiar el estado a no girando
-        spinButton.prop("disabled", false); // Habilitar el botón de nuevo
-        showWinner(); // Mostrar el ítem ganador
-      }, spinDuration); // Ejecutar el temporizador según la duración del giro
-    }
-  }
-
-  // Función para mostrar el ítem ganador
-  function showWinner() {
-    var winnerItem = roulette.find(".carousel-item.active"); // Encontrar el ítem activo que es el ganador
-    var winnerContent = winnerItem.find(".card").clone(); // Clonar el contenido del ítem ganador
-    winnerContent.css("transform", "rotate(0)"); // Quitar la rotación del contenido clonado
-    winnerModalBody.empty(); // Vaciar el cuerpo del modal
-    winnerModalBody.append(winnerContent); // Añadir el contenido clonado al cuerpo del modal
-    winnerModal.modal("show"); // Mostrar el modal
-  }
-
-  // Evento para girar la ruleta al pulsar el botón
-  spinButton.on("click", function() {
-    spinRoulette();
-  });
-
-</script>
-
+        // Mostrar el modal del ganador después de que se detenga la animación
+        setTimeout(function() {
+            var winnerText = $('#carouselExampleControls .carousel-item.active img').attr('alt');
+            $('#winnerText').text('¡Has ganado ' + winnerText + '!');
+            $('#winnerModal').modal('show');
+        }, 4000); // Ajusta el valor de tiempo en milisegundos según la duración de la animación
+    });
+});</script>
 </body>
 </html>
