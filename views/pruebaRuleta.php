9c6f7615ec2c9d239415554d0318b05bb46c45e1
“John <johndoe@example.com>
2023-05-29 19:28:07 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -1,146 +1,93 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ruleta Bootstrap</title>
-  <!-- Agrega los estilos de Bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
-  <style>
-    .card {
-      width: 200px;
-      height: 300px;
-      margin: 10px;
-      text-align: center;
-      background-color: #f8f9fa;
-    }
-    
-    #modalWinner {
-      text-align: center;
-    }
-    
-    .carousel.slide .carousel-inner {
-      display: flex;
-    }
-    
-    .carousel.slide .carousel-item {
-      flex: 1 0 100%;
-      transition: transform 3s ease-in-out;
-    }
-    
-    .carousel.slide .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) {
-      transform: translateX(0);
-    }
-    
-    .carousel.slide .carousel-item-next:not(.carousel-item-left) {
-      transform: translateX(100%);
-    }
-    
-    .carousel.slide .carousel-item-prev:not(.carousel-item-right) {
-      transform: translateX(-100%);
-    }
-    
-    .carousel.slide .carousel-item-right,
-    .carousel.slide .carousel-item-left {
-      transform: translateX(0);
-    }
-  </style>
+  <title>Carrusel múltiple con tarjetas</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-<div class="container text-center my-3">
-    <h2 class="font-weight-light">Bootstrap Multi Slide Carousel</h2>
-    <div class="row mx-auto my-auto justify-content-center">
-        <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 1</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 2</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 3</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 4</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 5</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 6</div>
-                        </div>
-                    </div>
-                </div>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card">
+              <img src="imagen1.jpg" class="card-img-top" alt="Imagen 1">
+              <div class="card-body">
+                <h5 class="card-title">Tarjeta 1</h5>
+                <p class="card-text">Contenido de la tarjeta 1</p>
+              </div>
             </div>
-            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            </a>
-            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            </a>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+              <div class="card-body">
+                <h5 class="card-title">Tarjeta 2</h5>
+                <p class="card-text">Contenido de la tarjeta 2</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <img src="imagen3.jpg" class="card-img-top" alt="Imagen 3">
+              <div class="card-body">
+                <h5 class="card-title">Tarjeta 3</h5>
+                <p class="card-text">Contenido de la tarjeta 3</p>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    <h5 class="mt-2 fw-light">advances one slide at a time</h5>
+    <div class="carousel-item">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card">
+              <img src="imagen4.jpg" class="card-img-top" alt="Imagen 4">
+              <div class="card-body">
+                <h5 class="card-title">Tarjeta 4</h5>
+                <p class="card-text">Contenido de la tarjeta 4</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <img src="imagen5.jpg" class="card-img-top" alt="Imagen 5">
+              <div class="card-body">
+                <h5 class="card-title">Tarjeta 5</h5>
+                <p class="card-text">Contenido de la tarjeta 5</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <img src="imagen6.jpg" class="card-img-top" alt="Imagen 6">
+              <div class="card-body">
+                <h5 class="card-title">Tarjeta 6</h5>
+                <p class="card-text">Contenido de la tarjeta 6</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Siguiente</span>
+  </a>
 </div>
 
-<script>
-    let items = document.querySelectorAll('.carousel .carousel-item')
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-
-</script>
 </body>
 </html>
