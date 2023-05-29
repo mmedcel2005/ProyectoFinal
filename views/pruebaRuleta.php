1136e48fcd7cdb15942a794ff9ca5b44140b2915
“John <johndoe@example.com>
2023-05-29 20:52:06 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>Carrusel múltiple con desplazamiento de uno en uno</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 </head>
@@ -18,67 +18,36 @@
       </div>
     </div>
   </div>
-  <div class="item">
-    <div class="card">
-      <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
-      <div class="card-body">
-        <h5 class="card-title">Tarjeta 2</h5>
-        <p class="card-text">Contenido de la tarjeta 2</p>
+  <!-- Resto de las tarjetas -->
+</div>
+
+<button id="randomBtn" class="btn btn-primary">Mover Carrusel</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="myModalLabel">Modal Title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-    </div>
-  </div>
-  <div class="item">
-    <div class="card">
-      <img src="imagen3.jpg" class="card-img-top" alt="Imagen 3">
-      <div class="card-body">
-        <h5 class="card-title">Tarjeta 3</h5>
-        <p class="card-text">Contenido de la tarjeta 3</p>
+      <div class="modal-body">
+        <p>Modal content goes here.</p>
       </div>
-    </div>
-  </div>
-  <div class="item">
-    <div class="card">
-      <img src="imagen4.jpg" class="card-img-top" alt="Imagen 4">
-      <div class="card-body">
-        <h5 class="card-title">Tarjeta 4</h5>
-        <p class="card-text">Contenido de la tarjeta 4</p>
-      </div>
-    </div>
-  </div>
-  <div class="item">
-    <div class="card">
-      <img src="imagen5.jpg" class="card-img-top" alt="Imagen 5">
-      <div class="card-body">
-        <h5 class="card-title">Tarjeta 5</h5>
-        <p class="card-text">Contenido de la tarjeta 5</p>
-      </div>
-    </div>
-  </div>
-  <div class="item">
-    <div class="card">
-      <img src="imagen6.jpg" class="card-img-top" alt="Imagen 6">
-      <div class="card-body">
-        <h5 class="card-title">Tarjeta 6</h5>
-        <p class="card-text">Contenido de la tarjeta 7</p>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
 </div>
 
-<button id="randomBtn" class="btn btn-primary">Mover Carrusel</button>
-
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-  Abrir Modal
-</button>
-
-
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script>
   $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    var carousel = $('.owl-carousel').owlCarousel({
       loop: true,
       margin: 10,
       nav: true,
@@ -102,20 +71,12 @@
       }
     });
 
-   
-
-
-
     $('#randomBtn').click(function() {
-      var carousel = $('.owl-carousel').data('owl.carousel');
       var randomIndex = Math.floor(Math.random() * carousel.items().length);
       var currentIndex = carousel.relative(carousel.current());
-      var direction = 'next' ;
+      var direction = 'next';
       carousel.to(randomIndex, 500, direction);
-
     });
-
-    
   });
 </script>
 
