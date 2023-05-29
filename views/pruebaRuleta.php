1078b2557af713f0ace737f9c38c487384727545
“John <johndoe@example.com>
2023-05-29 17:23:36 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -43,6 +43,38 @@
       background-color: #efb810;
       font-weight: bold;
     }
+    .container {
+  margin-top: 50px;
+}
+
+h1 {
+  text-align: center;
+}
+
+.card {
+  width: 300px;
+  margin: 0 auto;
+}
+
+#spinBtn {
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+}
+
+.modal {
+  display: none;
+  text-align: center;
+}
+
+.modal-content {
+  margin-top: 20px;
+}
+
+#winnerText {
+  font-size: 24px;
+  font-weight: bold;
+}
   </style>
 </head>
 
@@ -83,6 +115,104 @@
   </header>
 
   <main>
+
+  <body>
+  <div class="container">
+    <h1>Ruleta con Bootstrap</h1>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Item 1</h5>
+              <p class="card-text">Descripción del item 1</p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Item 2</h5>
+              <p class="card-text">Descripción del item 2</p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Item 3</h5>
+              <p class="card-text">Descripción del item 3</p>
+            </div>
+          </div>
+        </div>
+        <!-- Agrega más tarjetas para más elementos de la ruleta -->
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div>
+    <button id="spinBtn" class="btn btn-primary">Girar</button>
+  </div>
+
+  <div id="winnerModal" class="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">¡Ganador!</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p id="winnerText"></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="script.js"></script>
+  <script>
+    $(document).ready(function() {
+  var carousel = $("#carouselExampleControls");
+  var modal = $("#winnerModal");
+
+  // Configurar opciones de Bootstrap Carousel
+  carousel.carousel({
+    interval: false
+  });
+
+  // Obtener el número total de elementos de la ruleta
+  var itemCount = carousel.find(".carousel-item").length;
+
+  // Agregar evento al botón de girar
+  $("#spinBtn").click(function() {
+    // Obtener un número aleatorio para determinar el ganador
+    var winnerIndex = Math.floor(Math.random() * itemCount);
+
+    // Detener la ruleta en el índice ganador
+    carousel.carousel(winnerIndex);
+
+    // Mostrar el modal del ganador después de que finalice la animación
+    setTimeout(function() {
+      var winnerTitle = carousel.find(".carousel-item").eq(winnerIndex).find(".card-title").text();
+      $("#winnerText").text("¡El ganador es: " + winnerTitle + "!");
+      modal.modal("show");
+    }, 1000); // Ajusta este tiempo de espera según la duración de la animación de giro
+  });
+});
+  </script>
+</body>
 
   </main>
   <!--------------- FOOTER  --------------->
