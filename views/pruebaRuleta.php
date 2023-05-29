bb1ed27fb4c51e40ddfb21fc5571952cd2da5043
“John <johndoe@example.com>
2023-05-29 19:25:02 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -47,119 +47,100 @@
   </style>
 </head>
 <body>
-  <div class="container">
-    <h1>Ruleta Bootstrap</h1>
-    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Elemento 1</h5>
-                  <p class="card-text">Descripción del elemento 1</p>
+<div class="container text-center my-3">
+    <h2 class="font-weight-light">Bootstrap Multi Slide Carousel</h2>
+    <div class="row mx-auto my-auto justify-content-center">
+        <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 1</div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Elemento 2</h5>
-                  <p class="card-text">Descripción del elemento 2</p>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 2</div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Elemento 3</h5>
-                  <p class="card-text">Descripción del elemento 3</p>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 3</div>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 4</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 5</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 6</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </a>
         </div>
-        <div class="carousel-item">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Elemento 4</h5>
-                  <p class="card-text">Descripción del elemento 4</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Elemento 5</h5>
-                  <p class="card-text">Descripción del elemento 5</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Elemento 6</h5>
-                  <p class="card-text">Descripción del elemento 6</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-    <button id="spinBtn" class="btn btn-primary mt-3">¡Girar!</button>
-  </div>
+    <h5 class="mt-2 fw-light">advances one slide at a time</h5>
+</div>
 
-  <!-- Agrega los scripts de Bootstrap y jQuery -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      // Inicializar el carrusel
-      $('#carouselExample').carousel({
-        interval: false // Deshabilitar el auto-giro
-      });
+<script>
+    let items = document.querySelectorAll('.carousel .carousel-item')
 
-      // Obtener las tarjetas del carrusel
-      var cards = $('.carousel-item .card');
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
 
-      // Manejar el evento click del botón
-      $('#spinBtn').click(function() {
-        // Generar un índice aleatorio para seleccionar una tarjeta
-        var randomIndex = Math.floor(Math.random() * cards.length);
-
-        // Detener el carrusel y mostrar el ganador después de 3 segundos
-        $('#carouselExample').carousel('pause');
-        setTimeout(function() {
-          // Mostrar el modal con el elemento ganador
-          var winner = cards[randomIndex].querySelector('.card-title').innerText;
-          $('#modalWinner').text('¡El ganador es: ' + winner + '!');
-          $('#winnerModal').modal('show');
-        }, 3000);
-
-        // Girar el carrusel
-        $('#carouselExample').carousel('next');
-      });
-    });
-  </script>
-
-  <!-- Modal para mostrar el elemento ganador -->
-  <div id="winnerModal" class="modal fade" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Elemento Ganador</h5>
-        </div>
-        <div class="modal-body" id="modalWinner">
-          ¡El ganador es: !
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        </div>
-      </div>
-    </div>
-  </div>
+</script>
 </body>
 </html>
