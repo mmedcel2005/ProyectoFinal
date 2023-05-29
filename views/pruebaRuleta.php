977c27c183b25989e707ed37e409faf88ad9115f
“John <johndoe@example.com>
2023-05-29 20:15:00 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -81,13 +81,14 @@
 
     $('#randomBtn').click(function() {
       var randomIndex = Math.floor(Math.random() * carousel.items().length);
-      carousel.trigger('to.owl.carousel', [randomIndex, 500]);
+      carousel.to(randomIndex, 500, true);
     });
 
     carousel.on('changed.owl.carousel', function(event) {
-      var currentElement = $(event.target).find('.owl-item').eq(event.item.index).find('.card');
-      var cardTitle = currentElement.find('.card-title').text();
-      var cardText = currentElement.find('.card-text').text();
+      var currentIndex = event.item.index;
+      var currentItem = $(event.target).find('.owl-item').eq(currentIndex).find('.card');
+      var cardTitle = currentItem.find('.card-title').text();
+      var cardText = currentItem.find('.card-text').text();
       $('#itemModalLabel').text(cardTitle);
       $('#itemContent').html('<p>' + cardText + '</p>');
       $('#itemModal').modal('show');
