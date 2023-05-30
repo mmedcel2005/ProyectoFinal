9e501ff0014332719a637a0c7b640d79bef96c2f
“John <johndoe@example.com>
2023-05-30 21:53:44 +0200

    Update pruebaRuleta.php

--- views/pruebaRuleta.php
+++ views/pruebaRuleta.php
@@ -257,5 +257,7 @@
             $('.box-roulette').roulette();
         });
     </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 </body>
 </html>
