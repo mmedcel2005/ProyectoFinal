65a65c30b420a30cc14a278f75375615e54549d5
“John <johndoe@example.com>
2023-05-30 19:57:03 +0200

    Update abrirCajaV.php

--- views/abrirCajaV.php
+++ views/abrirCajaV.php
@@ -194,7 +194,7 @@
 
     <section>
       <div class="container py-5 ">
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-5">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-5 bg-custom-sec">
           <?php
           foreach ($items as $item) {
             print('<div class="col mb-4">');
