7f6c4524afa2d88796261d200baf1ad1a3643dee
“John <johndoe@example.com>
2023-05-31 14:11:24 +0200

    tokens compra

--- model/PackTokenM.php
+++ model/PackTokenM.php
@@ -11,24 +11,44 @@ use \PDOException;
 // Se define la clase Usuario
 class PackTokenM
 {
-// Método para obtener los registros de la tabla usuarios
-public function obtenerPackTokens($conexPDO)
-{
-    // Verifica si la conexión a la BD es válida
-    if ($conexPDO != null) {
-        try {
-            // Se define la consulta SQL
-            $sentencia = $conexPDO->prepare("SELECT * FROM proyecto.PackToken ORDER BY cantidadToken DESC ");
-            // Se ejecuta la sentencia SQL y se retornan los resultados
-            $sentencia->execute();
+    // Método para obtener los registros de la tabla usuarios
+    public function obtenerPackTokens($conexPDO)
+    {
+        // Verifica si la conexión a la BD es válida
+        if ($conexPDO != null) {
+            try {
+                // Se define la consulta SQL
+                $sentencia = $conexPDO->prepare("SELECT * FROM proyecto.PackToken ORDER BY cantidadToken DESC ");
+                // Se ejecuta la sentencia SQL y se retornan los resultados
+                $sentencia->execute();
 
-            // Devolvemos los datos en int
-            return $sentencia->fetchAll();
-        } catch (PDOException $e) {
-            // Si se produce un error, se muestra un mensaje en pantalla
-            print("Error al acceder a BD" . $e->getMessage());
+                // Devolvemos los datos en int
+                return $sentencia->fetchAll();
+            } catch (PDOException $e) {
+                // Si se produce un error, se muestra un mensaje en pantalla
+                print("Error al acceder a BD" . $e->getMessage());
+            }
         }
     }
-}
 
+    public function obtenerPackTokensPorID($idPackToken, $conexPDO)
+    {
+        // Verifica si la conexión a la BD es válida
+        if ($conexPDO != null && $idPackToken != null) {
+            try {
+                // Se define la consulta SQL
+                $sentencia = $conexPDO->prepare("SELECT * FROM proyecto.PackToken WHERE idPackToken= :idPackToken ");
+                // Se ejecuta la sentencia SQL y se retornan los resultados
+                $sentencia->bindParam(':idPackToken', $idPackToken);
+
+                $sentencia->execute();
+
+                // Devolvemos los datos en int
+                return $sentencia->fetchAll();
+            } catch (PDOException $e) {
+                // Si se produce un error, se muestra un mensaje en pantalla
+                print("Error al acceder a BD" . $e->getMessage());
+            }
+        }
+    }
 }
