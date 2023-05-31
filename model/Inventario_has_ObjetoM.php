157290f5150621fdeef4905f3543bb38cff5a804
“John <johndoe@example.com>
2023-05-31 21:10:45 +0200

    Create Inventario_has_ObjetoM.php

---
+++ model/Inventario_has_ObjetoM.php
@@ -0,0 +1,16 @@
+<?php
+// Se define el namespace al que pertenece la clase
+namespace model;
+
+require_once("Utils.php");
+
+// Se importan las clases PDO y PDOException
+use \PDO;
+use \PDOException;
+
+// Se define la clase Usuario
+class Inventario_has_ObjetoM
+{
+
+
+}
