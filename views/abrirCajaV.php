2ac1045a313e7289da5a9c7ab83d792a2417d624
“John <johndoe@example.com>
2023-05-31 18:38:31 +0200

    Update abrirCajaV.php

--- views/abrirCajaV.php
+++ views/abrirCajaV.php
@@ -315,11 +315,8 @@
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Has ganado:</h5>
+            <h5 class="modal-title" id="exampleModalLabel">No tienes suficientes tokens</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
