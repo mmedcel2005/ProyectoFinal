5788986b26a4e7cca832c6565922e6c629055816
“John <johndoe@example.com>
2023-05-31 18:57:01 +0200

    Update abrirCajaV.php

--- views/abrirCajaV.php
+++ views/abrirCajaV.php
@@ -290,24 +290,24 @@
     </section>
 
     <!-- Modal item -->
-    <div class="modal fade" id="itemGanado" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Has ganado:</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
+    <div class="modal fade" id="itemGanado" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Has ganado:</h5>
+      </div>
+      <div class="modal-body">
 
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Vender</button>
-            <input type="hidden" value="" id="idObjeto">
-            <button type="button" class="btn btn-primary">Guardar</button>
-          </div>
-        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Vender</button>
+        <input type="hidden" value="" id="idObjeto">
+        <button type="button" class="btn btn-primary">Guardar</button>
       </div>
     </div>
+  </div>
+</div>
+
 
 
     <!-- Modal NO TIENES SUFICIENTES TOKENS-->
