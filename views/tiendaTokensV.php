f9d7259050f7a79c15e639e3c45eae84d1634516
“John <johndoe@example.com>
2023-05-31 12:43:14 +0200

    Update tiendaTokensV.php

--- views/tiendaTokensV.php
+++ views/tiendaTokensV.php
@@ -45,13 +45,13 @@
     }
 
     tr:last-child td {
-  border-bottom: none;
-}
-.btn-amarillo:hover {
+      border-bottom: none;
+    }
+
+    .btn-amarillo:hover {
       background-color: #efb810;
       color: white;
     }
-    
   </style>
 </head>
 
@@ -109,7 +109,7 @@
             print('  <div class="card-body">');
             print('    <p class="card-text text-muted"><b>' . $tokens["precio"] . ' €</b></p>');
             print('    <h5 class="card-text text-token">' . $tokens["cantidadToken"] . '</h5>');
-            print('     <input type="hidden" id="idPackToken" name="idPackToken" value="' . $tokens["idPackToken"] . '">');
+            print('     <input type="hidden" id="idPackToken" name="id" value="' . $tokens["idPackToken"] . '">');
             print('     <button type="submit" data-bs-toggle="modal" data-bs-target="#myModal" class="btn btn-amarillo color-tokens col-sm-11">Comprar</button>');
             print('   </div>');
             print(' </div>');
@@ -122,76 +122,76 @@
     </article>
 
     <article>
-  <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Selecciona método de pago:</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Seleccionar</th>
-                <th scope="col">Nombre de la tarjeta</th>
-                <th scope="col">Números de la tarjeta</th>
-                <th scope="col">Titular de la tarjeta</th>
-                <th scope="col">Fecha de caducidad</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago1" onchange="handleCheckboxChange(1)"></td>
-                <td>Tarjeta 1</td>
-                <td>**** **** **** 1234</td>
-                <td>John Doe</td>
-                <td>05/25</td>
-              </tr>
-              <tr>
-                <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago2" onchange="handleCheckboxChange(2)"></td>
-                <td>Tarjeta 2</td>
-                <td>**** **** **** 5678</td>
-                <td>Jane Smith</td>
-                <td>10/23</td>
-              </tr>
-              <tr>
-                <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago3" onchange="handleCheckboxChange(3)"></td>
-                <td>Tarjeta 3</td>
-                <td>**** **** **** 9012</td>
-                <td>Mark Johnson</td>
-                <td>09/24</td>
-              </tr>
-              <tr>
-                <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago4" onchange="handleCheckboxChange(4)"></td>
-                <td>Tarjeta 4</td>
-                <td>**** **** **** 3456</td>
-                <td>Alice Johnson</td>
-                <td>12/26</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <input type="hidden" value="" id="idObjeto">
-          <button type="button" class="btn btn-primary">Guardar cambios</button>
+      <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Selecciona método de pago:</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Seleccionar</th>
+                    <th scope="col">Nombre de la tarjeta</th>
+                    <th scope="col">Números de la tarjeta</th>
+                    <th scope="col">Titular de la tarjeta</th>
+                    <th scope="col">Fecha de caducidad</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago1" onchange="handleCheckboxChange(1)"></td>
+                    <td>Tarjeta 1</td>
+                    <td>**** **** **** 1234</td>
+                    <td>John Doe</td>
+                    <td>05/25</td>
+                  </tr>
+                  <tr>
+                    <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago2" onchange="handleCheckboxChange(2)"></td>
+                    <td>Tarjeta 2</td>
+                    <td>**** **** **** 5678</td>
+                    <td>Jane Smith</td>
+                    <td>10/23</td>
+                  </tr>
+                  <tr>
+                    <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago3" onchange="handleCheckboxChange(3)"></td>
+                    <td>Tarjeta 3</td>
+                    <td>**** **** **** 9012</td>
+                    <td>Mark Johnson</td>
+                    <td>09/24</td>
+                  </tr>
+                  <tr>
+                    <td><input class="form-check-input" type="checkbox" name="metodoPago" id="metodoPago4" onchange="handleCheckboxChange(4)"></td>
+                    <td>Tarjeta 4</td>
+                    <td>**** **** **** 3456</td>
+                    <td>Alice Johnson</td>
+                    <td>12/26</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <input type="hidden" value="" id="idPackToken">
+              <button type="button" class="btn btn-primary">Guardar cambios</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</article>
+    </article>
 
-<script>
-  function handleCheckboxChange(checkboxNumber) {
-    // Desmarcar todos los checkboxes excepto el seleccionado
-    for (let i = 1; i <= 4; i++) {
-      if (i !== checkboxNumber) {
-        document.getElementById('metodoPago' + i).checked = false;
+    <script>
+      function handleCheckboxChange(checkboxNumber) {
+        // Desmarcar todos los checkboxes excepto el seleccionado
+        for (let i = 1; i <= 4; i++) {
+          if (i !== checkboxNumber) {
+            document.getElementById('metodoPago' + i).checked = false;
+          }
+        }
       }
-    }
-  }
-</script>
+    </script>
 
 
 
@@ -242,15 +242,29 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
-  function handleCheckboxChange(checkboxNumber) {
-    // Desmarcar todos los checkboxes excepto el seleccionado
-    for (let i = 1; i <= 4; i++) {
-      if (i !== checkboxNumber) {
-        document.getElementById('metodoPago' + i).checked = false;
+    // Obtener todos los botones "Comprar"
+    const buttons = document.querySelectorAll('.btn-amarillo');
+
+    // Agregar el evento click a cada botón
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Obtener el idPackToken de la tarjeta
+        const idPackToken = button.previousElementSibling.value;
+
+        // Establecer el idPackToken en el modal
+        document.getElementById('idPackToken').value = idPackToken;
+      });
+    });
+
+    function handleCheckboxChange(checkboxNumber) {
+      // Desmarcar todos los checkboxes excepto el seleccionado
+      for (let i = 1; i <= 4; i++) {
+        if (i !== checkboxNumber) {
+          document.getElementById('metodoPago' + i).checked = false;
+        }
       }
     }
-  }
-</script>
+  </script>
 </body>
 
 </html>
