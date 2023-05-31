6c3bd7f1d63abe7060f48e8ba1ce9b970de32357
“John <johndoe@example.com>
2023-05-31 20:24:13 +0200

    Update abrirCajaV.php

--- views/abrirCajaV.php
+++ views/abrirCajaV.php
@@ -302,8 +302,8 @@
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Vender</button>
         <form action="../controller/abrirCajaC.php" method="post">
-        <input type="hidden" value="<?php print($caja['idCaja']); ?>" id="idCaja">
-        <input type="hidden" value="" id="idObjeto">
+        <input type="hidden" value="<?php print($caja['idCaja']); ?>" name="idCaja" id="idCaja">
+        <input type="hidden" value="" name="idObjeto" id="idObjeto">
         <button type="submit" id="guardar" name="guardar" class="btn btn-primary">Guardar</button>
         </form>
       </div>
