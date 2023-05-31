a25d9aae4d53d07f190ccceb831245678a804167
“John <johndoe@example.com>
2023-05-31 20:30:00 +0200

    Update abrirCajaC.php

--- controller/abrirCajaC.php
+++ controller/abrirCajaC.php
@@ -47,6 +47,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 
                 $cambiarCantTokens = $gestorUsuario->cambiarCantidadTokens($cantTokensActual, $idUsuario, $conexPDO);
 
+                var_dump($cambiarCantTokens);
 
                 if ($cambiarCantTokens != false) {
                     $_SESSION['cantTokens'] = $cantTokensActual;
