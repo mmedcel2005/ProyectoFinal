b254bab2cf5b068d0465637b240d21661d0835b0
“John <johndoe@example.com>
2023-05-31 18:40:20 +0200

    Update abrirCajaV.php

--- views/abrirCajaV.php
+++ views/abrirCajaV.php
@@ -317,8 +317,6 @@
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">No tienes suficientes tokens</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
             <input type="hidden" value="" id="idObjeto">
             <form action="../controller/tiendaTokensC.php" method="post">
