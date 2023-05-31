0d436a9c3a9720cc4d6f9244d44a918dc6266e02
“John <johndoe@example.com>
2023-05-31 17:44:52 +0200

    Update abrirCajaV.php

--- views/abrirCajaV.php
+++ views/abrirCajaV.php
@@ -189,6 +189,49 @@
               print('</div>');
               print('</div>');
             }
+            foreach ($itemsAleatorio as $item) {
+              print('<div class="item">');
+              print('<div class="card" id="item-card-' . $item['idObjeto'] . '">');
+              print('    <div class="card-img-container">');
+              switch ($item["calidad"]) {
+                case "L":
+                  print('<img src="../src/img/bg-item-amarillo.png" class="card-img-top" alt="Imagen de fondo">');
+
+                  break;
+                case "E":
+                  print('<img src="../src/img/bg-item-rojo.png" class="card-img-top" alt="Imagen de fondo">');
+
+                  break;
+                case "SR":
+                  print('<img src="../src/img/bg-item-morado.png" class="card-img-top" alt="Imagen de fondo">');
+
+                  break;
+                case "R":
+                  print('<img src="../src/img/bg-item-azul.png" class="card-img-top" alt="Imagen de fondo">');
+
+                  break;
+                case "C":
+                  print('<img src="../src/img/bg-item-celeste.png" class="card-img-top" alt="Imagen de fondo">');
+
+                  break;
+                case "MC":
+                  print('<img src="../src/img/bg-item-gris.png" class="card-img-top" alt="Imagen de fondo">');
+
+                  break;
+                default:
+                  break;
+              }
+              print('        <div class="item-overlay">');
+              print('       <img src="' . $item['imagen'] . '" alt="Imagen de ' . $item['nombre'] . '" class="item-image img-fluid">');
+              print('           <br>');
+              print('      <br><h6 class="item-card-title text-white">' . $item['nombre'] . '</h6>');
+              print('      <input type="hidden" id="id" name="id" value="' . $item['idObjeto'] . '">');
+              print('       <h4 class="item-card-text text-token">' . $item['precio'] . '</h4>');
+              print('        </div>');
+              print('    </div>');
+              print('</div>');
+              print('</div>');
+            }
             ?>
           </div>
 
@@ -344,7 +387,7 @@
 
       $('#randomBtn').click(function() {
         var carousel = $('.owl-carousel').data('owl.carousel');
-        var randomIndex = Math.floor(Math.random() * (carousel.items().length - 10)) + 10;
+        var randomIndex = Math.floor(Math.random() * (carousel.items().length));
         var currentIndex = carousel.relative(carousel.current());
         var direction = 'next';
 
