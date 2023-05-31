ed5b469749782477683ee99f02218a692147607f
“John <johndoe@example.com>
2023-05-31 15:14:07 +0200

    Update tiendaTokensC.php

--- controller/tiendaTokensC.php
+++ controller/tiendaTokensC.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace controller;
 
 
 use \model\UsuarioM;
 use \model\Utils;
-use \model\CajasM; 
-use \model\PacktokenM; 
+use \model\CajasM;
+use \model\PacktokenM;
 //Creamos un array para guardar los datos del cliente
 
 
@@ -23,31 +24,34 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 
 
     if (isset($_POST["comprar"]) && $_POST["comprar"] == "comprar" && isset($_POST["idPackToken"]) && $_POST["idPackToken"] != null) {
-        $idPackToken=$_POST["idPackToken"];
+        $idPackToken = $_POST["idPackToken"];
 
         $packTokensComprado = $gestorToken->obtenerTokensPorID($idPackToken, $conexPDO);
 
-        $_SESSION['cantTokens'] = $_SESSION['cantTokens'] + $packTokensComprado["cantidadToken"];
+        if ($packTokensComprado != null) {
+
+            $_SESSION['cantTokens'] = $_SESSION['cantTokens'] + $packTokensComprado["cantidadToken"];
+        }
     }
+    var_dump($_SESSION["cantTokens"]);
+    var_dump($_POST["idPackToken"]);
 
 
-    $conexPDO = Utils::conectar($l=false);
+    $conexPDO = Utils::conectar($l = false);
 
     $packTokens = $gestorToken->obtenerPackTokens($conexPDO);
 
 
-    if($packTokens != null){
+    if ($packTokens != null) {
 
         include("../views/tiendaTokensV.php");
-
-    }else{
+    } else {
         $gestorCaja = new CajasM();
 
         $datosCajas = $gestorCaja->obtenerCajas($conexPDO);
 
         include("../views/inicioV.php");
     }
-
 } else {
     //Si entra aqui quiere decir que no se ha pulsado ningun boton dirigimos a la pagina de login
     include("../views/loginV.php");
