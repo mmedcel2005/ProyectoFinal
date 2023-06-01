a19521947102f7304b668836ff01f06364a0c68d
“John <johndoe@example.com>
2023-06-01 18:30:10 +0200

    Update pruebaEditarPerfil.php

--- views/pruebaEditarPerfil.php
+++ views/pruebaEditarPerfil.php
@@ -46,46 +46,38 @@
       </div>
       <div class="form-group">
         <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="tarjeta" name="tarjeta">
-          <label class="custom-control-label" for="tarjeta">Seleccionar tarjeta</label>
+          <input type="checkbox" class="custom-control-input" id="tarjeta1" name="tarjeta" onclick="uncheckOtherCheckboxes(this)">
+          <label class="custom-control-label" for="tarjeta1">Tarjeta 1</label>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" class="custom-control-input" id="tarjeta2" name="tarjeta" onclick="uncheckOtherCheckboxes(this)">
+          <label class="custom-control-label" for="tarjeta2">Tarjeta 2</label>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" class="custom-control-input" id="tarjeta3" name="tarjeta" onclick="uncheckOtherCheckboxes(this)">
+          <label class="custom-control-label" for="tarjeta3">Tarjeta 3</label>
         </div>
       </div>
       <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
   </div>
 
-  <div class="container mt-5">
-    <h2>Tarjetas con imágenes</h2>
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card">
-          <img src="imagen1.jpg" class="card-img-top" alt="Imagen 1">
-          <div class="card-body">
-            <h5 class="card-title">Tarjeta 1</h5>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
-          <div class="card-body">
-            <h5 class="card-title">Tarjeta 2</h5>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <img src="imagen3.jpg" class="card-img-top" alt="Imagen 3">
-          <div class="card-body">
-            <h5 class="card-title">Tarjeta 3</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    function uncheckOtherCheckboxes(checkbox) {
+      const checkboxes = document.getElementsByName("tarjeta");
+      for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] !== checkbox) {
+          checkboxes[i].checked = false;
+        }
+      }
+    }
+  </script>
 </body>
 </html>
