ab880b06ec9d96c057a1ff8107504c6acf2a2b9e
“John <johndoe@example.com>
2023-06-02 18:12:56 +0200

    Update usuarioV.php

--- views/usuarioV.php
+++ views/usuarioV.php
@@ -355,11 +355,9 @@
         </section>
 
         <?php
-    if ($notificacion == "ok") {
       print('<div id="notification" class="notification">');
       print('<span id="notification-message" class="notification-message"></span>');
       print('</div>');
-    }
 
 
     ?>
