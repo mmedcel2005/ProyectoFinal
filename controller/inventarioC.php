5740fb30c648477a24907467c24c536824238218
“John <johndoe@example.com>
2023-06-02 20:47:16 +0200

    Update inventarioC.php

--- controller/inventarioC.php
+++ controller/inventarioC.php
@@ -40,10 +40,6 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && isset($_SES
 
             $idUsuario = $_SESSION['idUsuario'];
 
-
-            var_dump($_SESSION['idInventario']);
-            var_dump($_POST['idObjeto']);
-
             if (isset($_SESSION['idInventario']) && $_SESSION['idInventario'] != null && isset($_POST['idObjeto']) && $_POST['idObjeto'] != null) {
 
                 $idInventario = $_SESSION['idInventario'];
