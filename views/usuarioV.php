110e91f8d96bcacdc4bf2ab6fb31e7543a0dacdc
“John <johndoe@example.com>
2023-06-02 18:28:47 +0200

    Update usuarioV.php

--- views/usuarioV.php
+++ views/usuarioV.php
@@ -406,8 +406,8 @@
             <a class="text-white" href="https://mdbootstrap.com/">Manuel Medina</a>
         </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         $(document).ready(function() {
             // Obtén la referencia del elemento de la ventana emergente
