50ad64da160333f3b9a7fe4c919c7afeef32c996
“John <johndoe@example.com>
2023-06-04 13:29:51 +0200

    Update registroC.php

--- controller/registroC.php
+++ controller/registroC.php
@@ -59,7 +59,7 @@ if (isset($_POST["nombre"]) && isset($_POST["apellidos"]) && isset($_POST["corre
 
 
         var_dump($anadirUsu);
-        var_dump($gestorInv);
+        var_dump($anadirInv);
 
         if ($anadirUsu != null && $anadirInv != null) {
 
