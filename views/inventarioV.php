591ecdab5edd2a62f8f0f45f5df85d73db3bca66
“John <johndoe@example.com>
2023-06-07 20:50:22 +0200

    Update inventarioV.php

--- views/inventarioV.php
+++ views/inventarioV.php
@@ -169,11 +169,11 @@
             print('     <div class="item-overlay">');
             print('       <img src="' . $item['imagen'] . '" alt="Imagen de' . $item['nombre'] . '" class="item-image img-fluid">');
             print('<button class="btn" onclick="enviar(this)" type="button" value="' . $item['idObjeto'] . '">');
-            print('<img src="../src/img/vender.png" alt="Icono vender" class="rounded-circle usuario-imagen" style="max-width: 20px;" />');
+            print('    <i class="bi bi-send-fill text-white"></i>');
             print('</button>');
 
             print('<button class="btn" onclick="vender(this)" type="button" value="' . $item['idObjeto'] . '">');
-            print('    <i class="bi bi-heart-fill text-white"></i>');
+            print('<img src="../src/img/vender.png" alt="Icono vender"style="max-width: 20px;" />');
             print('</button>');
             print('      <br><h6 class="item-card-title text-white">' . $item['nombre'] . '</h6>');
             print('       <h4 class="item-card-text text-token">' . $item['precio'] . '</h4>');
