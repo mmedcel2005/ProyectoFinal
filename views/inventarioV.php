709457878c75f10d49f71afedd405a0d652b449d
“John <johndoe@example.com>
2023-06-07 20:34:45 +0200

    Update inventarioV.php

--- views/inventarioV.php
+++ views/inventarioV.php
@@ -167,6 +167,7 @@
                 break;
             }
             print('     <div class="item-overlay">');
+            print('       <img src="' . $item['imagen'] . '" alt="Imagen de' . $item['nombre'] . '" class="item-image img-fluid">');
             print('<button class="btn" onclick="enviar(this)" type="button" value="' . $item['idObjeto'] . '">');
             print('    <i class="bi bi-send-fill text-white"></i>');
             print('</button>');
@@ -174,7 +175,6 @@
             print('<button class="btn" onclick="vender(this)" type="button" value="' . $item['idObjeto'] . '">');
             print('    <i class="bi bi-heart-fill text-white"></i>');
             print('</button>');
-            print('       <img src="' . $item['imagen'] . '" alt="Imagen de' . $item['nombre'] . '" class="item-image img-fluid">');
             print('      <br><h6 class="item-card-title text-white">' . $item['nombre'] . '</h6>');
             print('       <h4 class="item-card-text text-token">' . $item['precio'] . '</h4>');
             print('      </div>');
