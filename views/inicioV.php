f7a3627ae8d700afae284b9c0625c2a03ecb9964
“John <johndoe@example.com>
2023-06-08 19:50:05 +0200

    Update inicioV.php

--- views/inicioV.php
+++ views/inicioV.php
@@ -87,7 +87,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="../index.php"><b>Mistery Boxes</b></a>
+              <a class="nav-link active" href="../index.php"><b>Mistery Boxes</b></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#"><b>Oro gratis</b></a>
