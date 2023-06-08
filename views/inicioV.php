b4727320bd8f88be40f1ed4b78670584b2d306ae
“John <johndoe@example.com>
2023-06-08 19:13:07 +0200

    Update inicioV.php

--- views/inicioV.php
+++ views/inicioV.php
@@ -179,6 +179,11 @@
 
     <article>
       <div class="container">
+      <div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+  <div class="spinner-border" role="status">
+    <span class="visually-hidden">Cargando...</span>
+  </div>
+</div>
         <div class="row d-none" id="cajas">
 
 
